0e04d403eb4774e95b43b23610e0b8b849a42bd0
Rajdeep Saha <raj.x.saha@gmail.com>
2022-05-04 23:02:46 -0400

    Create newvpc.tf

---
+++ terraform/newvpc.tf
@@ -0,0 +1,9 @@
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "demo-vpc-terraform"
+    Purpose = "Jenkins Terraform Demo"
+  }
+}
