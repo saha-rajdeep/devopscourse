23cf8d929b03f9d29aac90f15259bfe18d138368
solkoff <sergey_volkoff@yahoo.com>
2022-10-24 13:23:52 -0700

    Update sayhello.sh

--- scripts/sayhello.sh
+++ scripts/sayhello.sh
@@ -1,6 +1,6 @@
 sleep 1 #Pause for 1 second. It's good to stop and smell the roses time to time
 if [ $# -ne 2 ]; then #number of input parameters should be 2
-    echo "Enter topic, target "
+    echo "Enter topic and target "
     exit 0
 else
     TOPIC_NAME=$1
